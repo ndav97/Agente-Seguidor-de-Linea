2f02836dcc26a8a6cb3d1aa36f097e4957fb9db2
Kevin Felix Chipana Chucare <kvn.chipana@gmail.com>
2025-04-30 19:20:23 -0500

    rebase en simulador.py

--- simulador.py
+++ simulador.py
@@ -25,115 +25,11 @@ class Simulador:
         self.contador_choques = 0
         self.reglas_usadas = set()
 
-<<<<<<< HEAD
-=======
-    def decidir_accion(self, percepcion):
-        contacto = percepcion['contacto']
-        camaras = percepcion['camaras_frontales']
-
-        izquierda = camaras['izquierda']
-        centro = camaras['centro']
-        derecha = camaras['derecha']
-
-        # Regla 1: Choque
-        if contacto:
-            giro = random.choice(['rotar_izquierda', 'rotar_derecha'])
-            return [giro], 'Regla_1_Choque_GiroAleatorio'
-
-        # Regla 2: Centro adelante es pared y derecha es oscura
-        if centro == 'pared' and derecha == 'oscura':
-            return ['rotar_derecha'], 'Regla_2_CentroPared_DerechaOscura'
-
-        # Regla 3: Izquierda oscura, centro pared, derecha blanca
-        if izquierda == 'oscura' and centro == 'pared' and derecha == 'blanca':
-            return ['rotar_izquierda'], 'Regla_3_IzqOscura_CentroPared'
-
-        # Regla 4: Izquierda oscura, centro pared, derecha oscura
-        if izquierda == 'oscura' and centro == 'pared' and derecha == 'oscura':
-            giro = random.choice(['rotar_izquierda', 'rotar_derecha'])
-            return [giro], 'Regla_4_IzqDerOscura_CentroPared'
-
-        # Regla 5: Todo pared
-        if izquierda == 'pared' and centro == 'pared' and derecha == 'pared':
-            giro = random.choice(['rotar_izquierda', 'rotar_derecha'])
-            return [giro], 'Regla_5_TodoPared'
-
-        # Regla 6: Esquina izquierda
-        if izquierda == 'pared' and centro == 'pared' and derecha == 'blanca':
-            return ['rotar_derecha'], 'Regla_6_EsquinaIzquierda'
-
-        # Regla 7: Esquina derecha
-        if izquierda == 'blanca' and centro == 'pared' and derecha == 'pared':
-            return ['rotar_izquierda'], 'Regla_7_EsquinaDerecha'
-
-        # Regla 8: Centro pared, lados blancos
-        if izquierda == 'blanca' and centro == 'pared' and derecha == 'blanca':
-            giro = random.choice(['rotar_izquierda', 'rotar_derecha'])
-            return [giro], 'Regla_8_CentroPared_LadosBlancos'
-
-        # Regla 9: Centro oscura
-        if centro == 'oscura':
-            return ['avanzar'], 'Regla_9_CentroOscura'
-
-        # Regla 10: Centro blanca, derecha oscura
-        if centro == 'blanca' and derecha == 'oscura':
-            return ['avanzar', 'rotar_derecha'], 'Regla_10_DerechaOscura'
-
-        # Regla 11: Centro blanca, izquierda oscura
-        if centro == 'blanca' and izquierda == 'oscura':
-            return ['avanzar', 'rotar_izquierda'], 'Regla_11_IzquierdaOscura'
-
-        # Regla 12: Centro blanca, lados oscuros
-        if centro == 'blanca' and izquierda == 'oscura' and derecha == 'oscura':
-            giro = random.choice(['rotar_izquierda', 'rotar_derecha'])
-            return ['avanzar', giro], 'Regla_12_CaminoBlancoTotal'
-
-        # Regla 13: Centro blanca, pared en ambos lados
-        if centro == 'blanca' and izquierda == 'pared' and derecha == 'pared':
-            return ['avanzar'], 'Regla_13_ParedIzquierda'
-
-        # Regla 14: Centro blanca, pared izquierda, derecha blanca
-        if centro == 'blanca' and izquierda == 'pared' and derecha == 'blanca':
-            accion = random.choice([
-                ['avanzar'],
-                ['avanzar', 'rotar_derecha']
-            ])
-            return accion, 'Regla_14_AvanzarOR_AvanzarRotarDer'
-
-        # Regla 15: Centro blanca, izquierda blanca, derecha pared
-        if centro == 'blanca' and izquierda == 'blanca' and derecha == 'pared':
-            accion = random.choice([
-                ['avanzar'],
-                ['avanzar', 'rotar_izquierda']
-            ])
-            return accion, 'Regla_15_AvanzarOR_AvanzarRotarIzq'
-
-        # Regla 16: Centro blanca, lados blancos
-        if centro == 'blanca' and izquierda == 'blanca' and derecha == 'blanca':
-            accion = random.choice([
-                ['avanzar', 'rotar_izquierda'],
-                ['avanzar', 'rotar_derecha']
-            ])
-            return accion, 'Regla_16_BlancoTotal_Exploracion'
-
-        # Regla 17: Default avanzar si se puede
-        if centro != 'pared':
-            return ['avanzar'], 'Regla_17_Default_Avanzar'
-
-        # Regla 18: Default girar aleatoriamente si no se puede avanzar
-        giro = random.choice(['rotar_izquierda', 'rotar_derecha'])
-        return [giro], 'Regla_18_Default_GiroAleatorio'
-
->>>>>>> bc40a41 (Inicializando el proyecto de la tarea 3)
     def ejecutar(self):
         with open("recorrido_agente.txt", "w") as recorrido:
             while self.pasos_realizados < self.pasos_maximos and not self.bucle_detectado:
                 percepcion = self.agente.percepcion_actual()
-<<<<<<< HEAD
                 acciones, regla_disparada = decidir_accion(percepcion)
-=======
-                acciones, regla_disparada = self.decidir_accion(percepcion)
->>>>>>> bc40a41 (Inicializando el proyecto de la tarea 3)
 
                 for accion in acciones:
                     self.acciones_ejecutadas.add(accion)
@@ -217,11 +113,7 @@ class Simulador:
         resumen.append(f"% de acciones ejecutadas: {porcentaje_acciones_usadas:.2f}%")
         resumen.append("\n--- Uso de Reglas ---")
         for regla, veces in self.reglas_contador.items():
-<<<<<<< HEAD
             resumen.append(f"{regla}: {veces} vez/veces")
-=======
-            resumen.append(f"{regla}: {veces} veces")
->>>>>>> bc40a41 (Inicializando el proyecto de la tarea 3)
         resumen.append("------------------------------------")
 
         for linea in resumen:
@@ -229,8 +121,4 @@ class Simulador:
 
         with open("resultado_simulacion.txt", "w") as archivo:
             for linea in resumen:
-<<<<<<< HEAD
                 archivo.write(linea + "\n")
-=======
-                archivo.write(linea + "\n")
->>>>>>> bc40a41 (Inicializando el proyecto de la tarea 3)
